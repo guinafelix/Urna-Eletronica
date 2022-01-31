6f7b239ab517b484c6237572bb1516236aa05c3f
guilherme <oguilhermeleitefelix@gmail.com>
2022-01-31 18:53:30 -0300

    Final Screen

---
+++ etapas.js
@@ -0,0 +1,52 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'fulano de tal',
+                partido: 'ABC',
+                fotos:[
+                    {url:'vereador1.jpg', legenda:'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'beltrano da silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'vereador2.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ] 
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url:'prefeito1.jpg', legenda: 'Prefeito'},
+                    {url:'vice1.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos:[
+                    {url:'prefeito2.jpg', legenda: 'Prefeito'},
+                    {url:'vice2.jpg', legenda:'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+]
+
+
